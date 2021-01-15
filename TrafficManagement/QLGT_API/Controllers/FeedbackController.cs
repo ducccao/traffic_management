2be2f22197852a029731553e864e2d8e080069d6
Duc Cao <caovanducs@gmail.com>
2021-01-15 12:57:54 +0700

    Change Admin Project Normal Into MVC Project

---
+++ TrafficManagement/QLGT_API/Controllers/FeedbackController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLGT_API.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        // GET: Feedback
+        public ActionResult List()
+        {
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        public ActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
---
+++ TrafficManagement/QLGT_API/Controllers/ProfileController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLGT_API.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult All()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+
+
+
+
+
+
+    }
+}
